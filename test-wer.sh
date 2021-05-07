0c159eccb9d68a4b52e512c4bceec26a1795e63f
Matilda Ferguson <mfergus3@cs.swarthmore.edu>
2021-05-07 12:59:23 -0400

    polished

--- test-wer.sh
+++ test-wer.sh
@@ -1,2 +1,2 @@
-cat bhw.tests.txt | apertium -d . bhw-eng > bhw-eng.tests.txt
-apertium-eval-translator -r eng.tests.txt -t bhw-eng.tests.txt
+cat ../ling073-bhw-corpus/bhw.tests.txt | apertium -d . bhw-eng > bhw-eng.tests.txt
+apertium-eval-translator -r ../ling073-bhw-corpus/eng.tests.txt -t bhw-eng.tests.txt
